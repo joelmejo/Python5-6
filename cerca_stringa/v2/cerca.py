ae94bfccb9348bf059cc4e0770524edf6201a142
joelmejo <167082021+joelmejo@users.noreply.github.com>
2024-11-02 17:09:48 +0100

    done

--- cerca_stringa/v2/cerca.py
+++ cerca_stringa/v2/cerca.py
@@ -1,123 +1,175 @@
-import os, mmap, base64
+import base64
+# import mmap
+import os
+# import textract
+from docx import Document
 import PyPDF2
-import textract
 import requests
-from PIL import Image as PImage
+import shutil
+import re
 
 img_ext: list = [".png", ".jpeg", ".jpg"]
 
 with open('pgkey.txt', 'r') as file:
     google_api_key = file.read()
 
-base_url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-pro-exp-0827:generateContent?key="
-api_url = base_url + google_api_key
+base_url: str = "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-pro-exp-0827:generateContent?key="
+api_url: str = base_url + google_api_key
 
 # IMISSIONE DEI PARAMETRI
 
-sRoot = input("Inserisci la root directory: ")
-sStringaDaCercare = input("Inserisci la stringa da cercare: ")
-SOutDir = input("Inserici la directory di output: ")
-iNumFileTrovati = 0
+sRoot: str = input("Inserisci la root directory: ")
+sStringaDaCercare: str = input("Inserisci la stringa da cercare: ")
+SOutDir: str = input("Inserisci la directory di output: ")
+root_choice: str = input("Inserire nel nome del file il path assoluto(a) o relativo(r): ")
+iNumFileTrovati: int = 0
+
 
 # METODI
 
-def CercaStringaInFileName(sFile, sStringToSearch):
-    sFilename1 = sFile.lower()
-    sStringToSearch1 = sStringToSearch.lower()
-    print("Cerco {0} in {1}".format(sStringToSearch1, sFilename1))
-    iRet = sFilename1.find(sStringToSearch1)
-    if(iRet>=0):
-        print("Trovato!")
+def CercaStringaInFileName(sFile: str, sStringToSearch: str) -> bool:
+    sFilename1: str = sFile.lower()
+    sStringToSearch1: str = sStringToSearch.lower()
+    print(f"Cerco {sStringToSearch1} in {sFilename1}")
+    # iRet: int = sFilename1.find(sStringToSearch1)
+    # if (iRet >= 0):
+    #     print("Trovato!")
+    #     return True
+    if sStringToSearch1 in sFilename1:
         return True
     return False
 
-def CercaInTxt(sFile, sString):
-    sString = sString.lower()
-    try:
-        with open(sFile) as f:
-            s = mmap.mmap(f.fileno(), 0, access = mmap.ACCESS_READ)
-            sAppo = s.readline()
-            while len(sAppo) > 0:
-                SAppo = SAppo.lower()
-                if sAppo.find(sString.encode() != -1):
-                    return True
-                else:
-                    sAppo = s.readline()
-    except:
-        return False
 
-def CercaInFilePdf(sFile,sString):
+def CercaInTxt(sFile: str, sString: str) -> bool:
+    """
+        Ritorna un valore binario indicando se la stringa è presente all'interno di un file txt.
+
+            Parameters:
+                sFile (str): File dove cercare la stringa
+                sString (str): Stringa da cercare
+
+            Returns:
+                bool: True se la stringa è trovata nel file, False altrimenti.
+
+    """
+    sString: str = sString.lower()
+    # try:
+    #         with open(sFile) as f:
+    #             s = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)
+    #             sAppo = s.readline()
+    #             while len(sAppo) > 0:
+    #                 SAppo = SAppo.lower()
+    #                 if sAppo.find(sString.encode() != -1):
+    #                     return True
+    #                 else:
+    #                     sAppo = s.readline()
+    #     except:
+    #         return False
+    try:
+        with open(sFile, encoding="utf-8") as f:
+            lines: list[str] = f.readlines()
+        for line in lines:
+            if sString in line:
+                return True
+        return False
+    except Exception as e:
+        print(e)
+
+def CercaInFilePdf(sFile: str, sString: str) -> bool:
     object = PyPDF2.PdfReader(sFile)
     numPages = len(object.pages)
     for i in range(0, numPages):
         pageObj = object.pages[i]
         text = pageObj.extract_text()
         text = text.lower()
-        if(text.find(sString)!=-1):
-           return True
+        if (text.find(sString) != -1):
+            return True
     return False
 
-def CercaInFileDocx(sFile,sString):
-    text = textract.process(sFile)
-    text = text.lower()
-    if(text.find(sString.encode())!=-1):
-        return True
+
+# def CercaInFileDocx(sFile: str, sString: str) -> bool:
+#    text = textract.process(sFile)
+#    text = text.lower()
+#    if (text.find(sString.encode()) != -1):
+#        return True
+#    return False
+
+def CercaInFileDocx(sFile: str, sString: str) -> bool:
+    doc = Document(sFile)
+    for para in doc.paragraphs:
+        if sString in para.text:
+            return True
     return False
 
-def CercaInImg(sImage,sString):
+def CercaInImg(sImage, sString) -> bool | None:
     with open(sImage, 'rb') as f:
         img = f.read()
-    jsonDataRequest = {
-    "contents":[
-        {
-        "parts":[
-            {"text": "Questa immagine è correlata alla parola" + sString + "? Rispondimi solamente con True o False."},
+        
+    jsonDataRequest: dict = {
+        "contents": [
             {
-            "inline_data": {
-                "mime_type":"image/jpeg",
-                "data": base64.b64encode(img).decode('utf-8')
-            }
+                "parts": [
+                    {
+                        "text": "Questa immagine è correlata alla parola" + sString + "? Rispondimi solamente con True o False."},
+                    {
+                        "inline_data": {
+                            "mime_type": "image/jpeg",
+                            "data": base64.b64encode(img).decode('utf-8')
+                        }
+                    }
+                ]
             }
         ]
-        }
-    ]
     }
     response = requests.post(api_url, json=jsonDataRequest)
     if response.status_code == 200:
         jsonfile = response.json()
-        risposta = jsonfile["candidates"][0]["content"]["parts"][0]["text"]
-        print(risposta)
-        if risposta == "True.":
+        risposta: str = jsonfile["candidates"][0]["content"]["parts"][0]["text"]
+        print("Gemini dice " + risposta)
+        risposta = risposta.strip('.')
+        if risposta == "True":
             return True
         else:
             return False
     print(response.status_code)
-    return "La richiesta a restituito un errore"
+    print("La richiesta ha restituito un errore")
 
 
-#NAVIGA NEL FILE SYSTEM
+# NAVIGA NEL FILE SYSTEM
 
 for root, dirs, files in os.walk(sRoot):
-    sToPrint = "Dir corrente {0} contenent {1} subdir e {2} files".format(root,len(dirs),len(files))
+    if root == SOutDir:
+        continue
+    sToPrint = f"Dir corrente {root} contenente {len(dirs)} subdir e {len(files)} files"
     print(sToPrint)
 
     for filename in files:
-        pathCompleto = os.path.join(root,filename)
-        sOutFileName,sOutFileExt = os.path.splitext(filename)
-        iRet = CercaStringaInFileName(filename, sStringaDaCercare)
-        if iRet == True:
-            print("Trovato file: ", filename)
-            iNumFileTrovati += 1
-        elif sOutFileExt.lower()==".pdf":
-            if CercaInFilePdf(pathCompleto,sStringaDaCercare):
-                iNumFileTrovati += 1
-        elif sOutFileExt.lower()==".docx":
-            if CercaInFileDocx(pathCompleto,sStringaDaCercare):
-                iNumFileTrovati += 1
+        stringfound: bool = False
+        pathCompleto = os.path.join(root, filename)
+        sOutFileName, sOutFileExt = os.path.splitext(filename)
+        if CercaStringaInFileName(filename, sStringaDaCercare):
+            stringfound = True
+        elif sOutFileExt.lower() == ".pdf":
+            if CercaInFilePdf(pathCompleto, sStringaDaCercare):
+                stringfound = True
+        elif sOutFileExt.lower() == ".docx":
+            if CercaInFileDocx(pathCompleto, sStringaDaCercare):
+                stringfound = True
         elif any(sOutFileExt.lower() == ext for ext in img_ext):
-            if CercaInImg(pathCompleto,sStringaDaCercare):
-                iNumFileTrovati += 1
-        elif sOutFileExt.lower()==".txt":
-            if CercaInTxt(pathCompleto,sStringaDaCercare):
-                iNumFileTrovati += 1
-print(iNumFileTrovati)
+            if CercaInImg(pathCompleto, sStringaDaCercare):
+                stringfound = True
+        elif sOutFileExt.lower() == ".txt":
+            if CercaInTxt(pathCompleto, sStringaDaCercare):
+                stringfound = True
+        if stringfound:
+            print("Trovato file: ", filename)
+            os.makedirs(SOutDir, exist_ok=True)
+            if root_choice.lower() == 'a':
+                absolute_root = os.path.abspath(root)
+                sanitized_root = re.sub(r'[\\/]', '_', absolute_root)
+            else:
+                relative_root = os.path.relpath(root, sRoot)
+                sanitized_root = re.sub(r'[\\/]', '_', relative_root)
+            new_file_name: str = f"{sanitized_root}_{filename}"
+            dest_path = os.path.join(SOutDir, new_file_name)
+            shutil.copy(pathCompleto, dest_path)
